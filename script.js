ac1a845c01242b4e80ee484d559959f57cd5e26a
Arthur Albuquerque <arthur007zinho@hotmail.com>
2022-04-26 14:13:40 -0300

    css update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var btn = document.querySelector('#btn')
+btn.addEventListener('click', calcular)
+
 function calcular() {
     var nt1 = document.querySelector('#notaProva');
     var nt2 = document.querySelector('#notaPim');
@@ -5,8 +8,10 @@ function calcular() {
     var media = document.querySelector('#media');
     var res = Number(nt1.value) * 0.7 + Number(nt2.value) * 0.2 + Number(nt3.value) * 0.1;
     if(res >= 5.8) {
-        media.innerHTML = `Sua média foi ${res.toFixed(2)}.<br/>E você <strong>PASSOU</strong>!`
-    } else {
-        media.innerHTML = `Sua média foi ${res.toFixed(2)}.<br/>E você <strong>REPROVOU!</strong>`
+        media.innerHTML = `Sua média foi <strong>${res.toFixed(2)}.<br/>Você <strong>PASSOU</strong>!`
+    } else if (res == 0){
+        media.innerHTML = `Digite os valores!`
+    }else {
+        media.innerHTML = `Sua média foi <strong>${res.toFixed(2)}</strong>.<br/>Você <strong>REPROVOU!</strong>`
     }
-}
+}   
